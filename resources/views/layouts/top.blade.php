6f5224e2266c4f851478a7403c665c4d58731fbe
Drima <drima@ua.fm>
2018-12-06 23:30:02 +0200

    Add more light on tower

--- resources/views/layouts/top.blade.php
+++ resources/views/layouts/top.blade.php
@@ -1,5 +1,5 @@
 <div class="jumbotron text-center top-heading">
-    <img src="/images/tower.png" class="top-tower">
+    <img src="/images/tower2.png" class="top-tower">
     <h1>СВЕТОВАЯ БАШНЯ</h1>
     <img src="/images/logo.png">
     <h4>ОСВЕТИ СВОЮ ЖИЗНЬ</h4>
