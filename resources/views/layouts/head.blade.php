68b5354f0f447d97a945477d5939a701db644756
Drima <drima@ua.fm>
2018-12-09 15:37:21 +0200

    New font in top

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/common.css?ver=0.05">
     <link rel="stylesheet" href="/css/mix.css">
-    <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lobster|El+Messiri" rel="stylesheet">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
