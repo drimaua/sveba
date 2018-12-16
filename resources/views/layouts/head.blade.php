60990eaefe114a0eff4d2372397da9bd6144ba1d
Drima <drima@ua.fm>
2018-12-16 17:49:33 +0200

    Css ver up

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -4,14 +4,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/css/common.css?ver=0.05">
+    <link rel="stylesheet" href="/css/common.css?ver=0.06">
     <link rel="stylesheet" href="/css/mix.css">
     <link href="https://fonts.googleapis.com/css?family=Lobster|El+Messiri" rel="stylesheet">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="/js/swiper.min.js?ver=0.05"></script>
+    <script type="text/javascript" src="/js/swiper.min.js?ver=0.06"></script>
     <style>
         .fakeimg {
             height: 200px;
