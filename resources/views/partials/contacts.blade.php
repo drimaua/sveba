b4bfb4bfd9b1d58bcaacfa9c221feb577dd47a1a
Drima <drima@ua.fm>
2018-12-21 14:55:21 +0200

    Text color + link.

--- resources/views/partials/contacts.blade.php
+++ resources/views/partials/contacts.blade.php
@@ -4,15 +4,15 @@
 <p>тел.: +38 (044) 465 67 12</p>
 <p>тел.: +38 (067) 328 40 94</p>
 <p>тел.: +38 (050) 546 96 54</p>
-<p>e-mail: <a class="link-style" href="mailto:expotorg_7@ukr.net">expotorg_7@ukr.net</a></p>
+<p>e-mail: <a class="link-black" href="mailto:expotorg_7@ukr.net">expotorg_7@ukr.net</a></p>
 <hr class="new-hr">
 <h4>{{ Lang::get('texts.producer') }}</h4>
 <h4>{{ Lang::get('texts.sveba') }}</h4>
 <p>Тел.: +7 (901) 518 34 74</p>
 <p>Факс: +7 (919) 967 11 01</p>
-<p>e-mail: <a class="link-style" href="mailto:info@sveba.ru">info@sveba.ru</a></p>
-<p>Web: <a class="link-style" href="http://www.sveba.ru">www.sveba.ru</a></p>
+<p>e-mail: <a class="link-black" href="mailto:info@sveba.ru">info@sveba.ru</a></p>
+<p>Web: <a class="link-black" href="http://www.sveba.ru">www.sveba.ru</a></p>
 <hr class="new-hr">
 <h2 class="description-heading description-heading-center">{{ Lang::get('menu.rent') }}</h2>
 <p>тел.: +38 (095) 114-82-05</p>
-<p>e-mail: <a class="link-style" href="mailto:light.tower2.0@gmail.com">light.tower2.0@gmail.com</a></p>
+<p>e-mail: <a class="link-black" href="mailto:light.tower2.0@gmail.com">light.tower2.0@gmail.com</a></p>
